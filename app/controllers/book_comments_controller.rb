ca3e8060c65d24abb8f6eca9ca6447df062ec10f
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-09-02 11:46:58 +0000

    [Finish] 応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,29 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = book.id
+    @book_comment.book_id = @book.id
+    @book_comments = BookComment.where(book_id: @book.id)
     if @book_comment.save
-      redirect_to book_path(book)
+       @book_comment = BookComment.new
+      #redirect_to book_path(book)
       #redirect_back fallback_location: root_path
     else
       @book = Book.find(params[:book_id])
       @new_book = Book.new
       @user = @book.user
       @book_comments = BookComment.where(book_id: @book.id)
-      render "books/show"
+      #render "books/show"
     end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+    @book_comments = BookComment.where(book_id: @book.id)
     book_comment = BookComment.find(params[:id])
     book_comment.destroy
-    redirect_back fallback_location: root_path
+    @book_comment = BookComment.new
+    #redirect_back fallback_location: root_path
   end
   
   private
